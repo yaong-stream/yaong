565f8cd2a97c595e81bdfe539c4fd14a1e4078fc
ByeongGi <wwww3426@naver.com>
2024-10-06 03:03:01 +0900

    feat: add typings(SupabaseClientType).

--- src/hooks/use-supabse.ts
+++ src/hooks/use-supabse.ts
@@ -1,9 +1,10 @@
 import { createClient } from "@/utils/supabase/client";
 import { useMemo } from "react";
 
+export type SupabaseClientType = ReturnType<typeof useSupabase>;
+
 function useSupabase() {
     return useMemo(createClient, []);
 }
-
 
 export { useSupabase };
